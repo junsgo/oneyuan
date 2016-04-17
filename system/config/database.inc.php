3c5e1bc21fc1319e9c3c2e61417bf4a3eb6dab17
jians <jians@localhost>
2016-04-17 10:29:35 +0800

    Signed-off-by:jians <jians@localhost>

---
+++ system/config/database.inc.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+'default' => array (
+'hostname' => '127.0.0.1','database' => 'wy','username' => 'root','password' => 'root','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
+);
